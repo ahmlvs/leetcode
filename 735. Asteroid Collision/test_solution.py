2e418c4b2ea993ad8e428a7406fd0ab9c5ee98ec
ahmelevs <andrejs.hmelovs@gmail.com>
2024-11-04 20:15:37 +0400

    setUp and tearDown for unittest

--- 735. Asteroid Collision/test_solution.py
+++ 735. Asteroid Collision/test_solution.py
@@ -3,11 +3,30 @@ from solution import Solution
 
 class TestSolution(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+    
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        self.solution = Solution()
+        print('setUp')
+    
+    def tearDown(self):
+        print('tearDown')
+
     def test_asteroidCollision(self):
-        solution = Solution()
-        self.assertEqual(solution.asteroidCollision([5,10,-5]), [5,10])
-        self.assertEqual(solution.asteroidCollision([8,-8]), [])
-        self.assertEqual(solution.asteroidCollision([10,2,-5]), [10])
-        self.assertEqual(solution.asteroidCollision([-2,-1,1,2]), [-2,-1,1,2])
-        self.assertEqual(solution.asteroidCollision([-2,-2,1,-2]), [-2,-2,-2])
+        print('test_asteroidCollision')
+        self.assertEqual(self.solution.asteroidCollision([5,10,-5]), [5,10])
+        self.assertEqual(self.solution.asteroidCollision([8,-8]), [])
+        self.assertEqual(self.solution.asteroidCollision([10,2,-5]), [10])
+        self.assertEqual(self.solution.asteroidCollision([-2,-1,1,2]), [-2,-1,1,2])
+        self.assertEqual(self.solution.asteroidCollision([-2,-2,1,-2]), [-2,-2,-2])
+
+
+if __name__ == '__main__':
+    unittest.main()
     
