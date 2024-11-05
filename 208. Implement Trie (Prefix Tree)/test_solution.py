65ccc61457d8fda2e53a0ef840379c4c1dfadaed
ahmelevs <andrejs.hmelovs@gmail.com>
2024-11-05 18:24:21 +0400

    50. Implement Trie (Prefix Tree)

---
+++ 208. Implement Trie (Prefix Tree)/test_solution.py
@@ -0,0 +1,16 @@
+import unittest
+from solution import Trie
+
+class TestSolution(unittest.TestCase):
+    def test_1(self) -> None:
+        trie = Trie()
+        trie.insert("apple")
+        self.assertEqual(trie.search("apple"), True)
+        self.assertEqual(trie.search("app"), False)
+        self.assertEqual(trie.startsWith("app"), True)
+        trie.insert("app")
+        self.assertEqual(trie.search("app"), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
